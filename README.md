06153eaeeead3c00bd1c768f5b2371f77891067f
fmothales <148076278+fmothales@users.noreply.github.com>
2023-10-20 15:28:55 +0200

    Ajout de specfiles

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # podman-spec
 Podman spec file
 
-See https://src.fedoraproject.org/rpms/podman and https://github.com/containers/podman.
+See:
+- https://src.fedoraproject.org/rpms/podman
+- https://git.centos.org/rpms/podman/tree/c8-stream-rhel8
+- https://git.almalinux.org/rpms/podman/src/branch/c8-stream-rhel8
+- https://github.com/containers/podman
 
 ## How to create patched spec file
 ```bash
--- patch.sh
+++ patch.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-SOURCE_SPEC="https://src.fedoraproject.org/rpms/podman/raw/rawhide/f/podman.spec"
+#SOURCE_SPEC="https://src.fedoraproject.org/rpms/podman/raw/rawhide/f/podman.spec"
+#SOURCE_SPEC="https://git.centos.org/rpms/podman/raw/c8-stream-rhel8/f/SPECS/podman.spec"
+SOURCE_SPEC="https://git.almalinux.org/rpms/podman/raw/branch/c8-stream-rhel8/SPECS/podman.spec"
+DNSNAME_COMMIT="18822f9a4fb35d1349eb256f4cd2bfd372474d84"
 
+wget -O dnsname-${DNSNAME_COMMIT}.tar.gz https://github.com/containers/dnsname/archive/${DNSNAME_COMMIT}.tar.gz
 wget -O podman.spec ${SOURCE_SPEC}
